c459747ce827ce8f4f45c9a26e4e76af366faa29
Amirkafi <doostkafiamir@gmail.com>
2025-11-13 20:53:50 +0330

    dockerize

--- Carpet/obj/Debug/net9.0/Carpet.AssemblyInfo.cs
+++ Carpet/obj/Debug/net9.0/Carpet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Carpet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c123b8b2c36b73810b88136d4cd9723edbaef254")]
 [assembly: System.Reflection.AssemblyProductAttribute("Carpet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Carpet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
